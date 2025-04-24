09c1c03d675d6491d1097c492a7ae28cc6852655
jorge_vieira <jorgelv21@gmail.com>
2025-04-24 19:37:10 -0300

    docs: documented generic service

--- src/main/java/com/example/ToDoList/services/GenericService.java
+++ src/main/java/com/example/ToDoList/services/GenericService.java
@@ -9,40 +9,79 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Generic Service.
+ * @param <T> - Generic Class
+ * @param <L> - Generic Number
+ */
 @Service
 public abstract class GenericService<T extends GenericClass, L extends Number> {
 
+    /**
+     * Generic repository instance
+     */
     @Autowired
     private final GenericRepository<T, L> genericRepository;
 
+    /**
+     * Generic Service Constructor
+     * @param genericRepository - Generic Repository
+     */
     protected GenericService(GenericRepository<T, L> genericRepository) {
         this.genericRepository = genericRepository;
     }
 
+    /**
+     * Find all data method
+     * @return Data list.
+     */
     public List<?> getAllData(){
         return genericRepository.findAll();
     }
 
+    /**
+     * Find data by id method.
+     * @param id - Data id
+     * @return Data
+     */
     public T getDataById(L id){
         return genericRepository.findById(id).orElseThrow(() -> new NotFoundException("Object Not Found"));
     }
 
+    /**
+     * Create data method.
+     * @param payload - Data payload
+     * @return New data
+     */
     public T createData(T payload){
         return genericRepository.save(payload);
     }
 
+    /**
+     * Update data method
+     * @param payload Data payload
+     * @param id - Data id
+     */
     public void updateData(T payload, L id){
         T object = getDataById(id);
         BeanUtils.copyProperties(payload, object, "id");
         genericRepository.saveAndFlush(object);
     }
 
+    /**
+     * Delete data logically method
+     * @param id - Data id.
+     */
     public void deleteDataLogically(L id){
         T object = getDataById(id);
         object.setDeleted(true);
         genericRepository.saveAndFlush(object);
     }
 
+    /**
+     * Delete Physically method.
+     * @param id - Data id.
+     */
     public void deleteDataPhysically(L id){
     T object = getDataById(id);
     genericRepository.delete(object);
